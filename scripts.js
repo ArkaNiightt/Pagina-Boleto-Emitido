a8ddbc1285863030d7f53fec7c39b4a2df0c63d9
ArkaNiightt <augustojoao60@yahoo.com>
2024-10-28 14:59:28 -0300

    reformat script.js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,22 @@
-// Função para extrair as informações de pedido, valor e vencimento
-document.addEventListener("DOMContentLoaded", function () {
-    // Selecionar o número do pedido
-    const pedido = document.querySelector(".transaction_token_PPCPMTB5FJC503").textContent.trim();
-  
-    // Selecionar o valor do pedido
-    const valor = document.querySelector(".btn.btn-outline-info").textContent.trim();
-  
-    // Selecionar a data de vencimento
-    const vencimento = document.querySelector(".btn.btn-outline-danger").textContent.trim();
-  
-    // Exibir as informações no console
-    console.log("Pedido: ", pedido);
-    console.log("Valor: ", valor);
-    console.log("Vencimento: ", vencimento);
-  
-    // Adicionar as informações no elemento HTML especificado
-    const infoContainer = document.querySelector(".bg-gray-50.p-4.rounded-lg.mb-6");
-    if (infoContainer) {
-      infoContainer.innerHTML = `
+
+// Selecionar o número do pedido
+const pedido = Math.random()
+
+// Selecionar o valor do pedido
+const valor = 299.00
+
+// Selecionar a data de vencimento
+const vencimento = new Date()
+
+// Exibir as informações no console
+console.log("Pedido: ", pedido);
+console.log("Valor: ", valor);
+console.log("Vencimento: ", vencimento);
+
+// Adicionar as informações no elemento HTML especificado
+const infoContainer = document.querySelector(".bg-gray-50.p-4.rounded-lg.mb-6");
+if (infoContainer) {
+    infoContainer.innerHTML = `
         <p class="text-sm text-gray-500">
           <span class="font-semibold">Número do Pedido:</span> ${pedido}
         </p>
@@ -28,6 +27,5 @@ document.addEventListener("DOMContentLoaded", function () {
           <span class="font-semibold">Vencimento:</span> ${vencimento}
         </p>
       `;
-    }
-  });
-  
+}
+
